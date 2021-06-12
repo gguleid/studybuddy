9d3dc33d390f84bd2c7513204261c0e13d038d10
gguleid <gguleid@gmail.com>
2021-06-12 15:14:07 -0400

    intial setup commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// Dependencies
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+
+
+// Listener 
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
