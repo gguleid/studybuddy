9f22652d29253c7cc5e25d3e916a7264fe248871
gguleid <gguleid@gmail.com>
2021-06-12 15:20:59 -0400

    configures database

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 // Dependencies
 const express = require('express');
+const  mongoose  = require('mongoose');
 const app = express();
 require('dotenv').config();
 
+// Database Configuration
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+// Database Connection Error / Success
+const db = mongoose.connection;
+db.on('connected', () => console.log('Mongo is connected'));
+db.on('disconnected', () => console.log('Mongo is disconnected'));
+db.on('error', (err) => console.log(err.message + 'is mongod is not running?'));
 
 
 // Listener 
