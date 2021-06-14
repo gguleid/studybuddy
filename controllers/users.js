ae8e81749472b14dad38385ca58567b3b29ce380
gguleid <gguleid@gmail.com>
2021-06-14 10:59:31 -0400

    feat: adds create route for user sign up

--- controllers/users.js
+++ controllers/users.js
@@ -8,6 +8,13 @@ const User = require('../models/user')
 // New (Sign up page)
 
 // Create (Sign up page)
+userRouter.post('/', (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(12));
+
+    User.create(req.body, (error, createdUser)=> {
+        res.send(createdUser);
+    });
+});
 
 // Export User Router
 module.exports = userRouter;
