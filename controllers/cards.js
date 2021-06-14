ecc9fc3ad2bc2f55bb6aa84a8fd32c17115cb862
gguleid <gguleid@gmail.com>
2021-06-14 17:28:09 -0400

    adds setup routes

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Card = require('../models/cards');
 
 // Index Route - Cards
 router.get('/', (req, res) => {
 	res.render('cards/index');
 });
+
 // New Route - New index cards
 router.get('/new', (req, res) => {
 	res.render('cards/new');
 });
+
+// Create route - Create Index Cards
+router.post('/', (req, res) => {
+	Card.create(req.body, (err, createdCard) => {
+		res.redirect('/cards');
+	});
+})
+
+
+// Show Route
+router.get('/', (req, res) => {
+	Card.find({}, (err, foundCards) => {
+		res.render('cards/index.ejs', {
+			cards: foundCards
+		});
+	});
+});
+
 module.exports = router;
